1302bb6017def178b5cdce04236fb884467b8550
Andrea Creazzi <andreacreazzi@libero.it>
2023-05-31 17:53:27 +0200

    add js (ex: Dadi complete)

--- js/dadi.js
+++ js/dadi.js
@@ -1,2 +1,30 @@
 // Controllo javascript
-console.log(`JS OK`)
+console.log(`JS OK`);
+
+// Recupero l'elemnto dal DOM
+
+const gameElement = document.getElementById(`game`);
+const resultElement = document.getElementById(`result`)
+
+// Dati Preliminari
+
+let message = ``
+
+// Metto in ascolto il bottone
+
+gameElement.addEventListener(`click` , function(){
+    const randomUser = Math.floor(Math.random() * 6) + 1;
+    const randomCpu = Math.floor(Math.random() * 6) + 1;
+    console.log(`il tuo numero user è `+  randomUser);
+    console.log(`il tuo numero CPU è ` + randomCpu);
+
+    if(randomUser < randomCpu){
+        message = `Ha vinto la CPU`
+    }else if(randomCpu < randomUser){
+        message = `ha vinto l'user`
+    }else if(randomCpu === randomUser){
+        message = `Pareggio`
+    }
+
+    resultElement.innerText = message
+})
